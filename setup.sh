3037306613f4b6abfd4042516befe691a5b2e6ed
Ben_Feldberg_Collins <b.feldberg.collins@accenture.com>
2023-08-16 19:33:30 +0100

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
-WD=$(pwd)
+WD=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+echo $WD
 if grep -q "export TOOLKIT_PATH=" ~/.zshrc; then
   sed -i '' -e '/export TOOLKIT_PATH=/d' ~/.zshrc
 fi
 
-  echo "export TOOLKIT_PATH="${WD}/typescript/src"" >> ~/.zshrc
+  echo "export TOOLKIT_PATH="${WD}/search"" >> ~/.zshrc
+  mkdir -p "$TOOLKIT_PATH/search"
+  cp -r ./typescript/src/ "$TOOLKIT_PATH/typescript/"
+  cp -r ./react/src/export/ "$TOOLKIT_PATH/react/"
 if ! test -f "/usr/local/bin/toolkit"; then
   echo "COPYING"
-  cp toolkit /usr/local/bin/toolkit && chmod u+x /usr/local/bin/toolkit
+  chmod u+x "$WD/toolkit" && ln -s "$WD/toolkit" /usr/local/bin/toolkit 
 fi
+
