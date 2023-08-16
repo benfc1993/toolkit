099cb9476277565052a6080924dc33354f339adc
benfc1993 <benfeldbergcollins@hotmail.co.uk>
2023-08-16 19:49:13 +0100

    add linux support

--- setup.sh
+++ setup.sh
@@ -3,13 +3,19 @@
 WD=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 echo $WD
 if grep -q "export TOOLKIT_PATH=" ~/.zshrc; then
-  sed -i '' -e '/export TOOLKIT_PATH=/d' ~/.zshrc
+if [[ "$OSTYPE" == "darwin"* ]]; 
+  then
+    sed -i '' -e '/export TOOLKIT_PATH=/d' ~/.zshrc
+  else
+    echo "Linux"
+    sed -i -e '/export TOOLKIT_PATH=/d' ~/.zshrc
+  fi
 fi
 
   echo "export TOOLKIT_PATH="${WD}/search"" >> ~/.zshrc
-  mkdir -p "$TOOLKIT_PATH/search"
-  cp -r ./typescript/src/ "$TOOLKIT_PATH/typescript/"
-  cp -r ./react/src/export/ "$TOOLKIT_PATH/react/"
+  mkdir -p "$TOOLKIT_PATH"
+  cp -r "${WD}/typescript/src/" "$TOOLKIT_PATH/typescript/"
+  cp -r "${WD}/react/src/export/" "$TOOLKIT_PATH/react/"
 if ! test -f "/usr/local/bin/toolkit"; then
   echo "COPYING"
   chmod u+x "$WD/toolkit" && ln -s "$WD/toolkit" /usr/local/bin/toolkit 
